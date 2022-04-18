4a5981be14efad3de56707ae2a912ef0dc00f921
Obianuju <obianujuofodu@gmail.com>
2022-04-18 16:05:42 +0100

    Add files via upload

---
+++ SH_NT_NL_DS.py
@@ -0,0 +1,12 @@
+# TRIPLE QUOTATION IS ALSO KNOWN AS DOCUMENT STRINGS.
+# THESE ARE HOW DOCSTRINGS ARE USED.
+print('''These are my cousin;
+            onyi
+            chidinm
+            chinenye''')
+
+# \n means new line while \t means new tab
+print('These are my cousins; \n\t\t\t'
+      'onyi \n\t\t\t'
+      'chidinm\n\t\t\t'
+      'chinenye')
